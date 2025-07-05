6b0c0431ea67d6067add848d2085ede6e52328af
Abu Sufian Mahin <abusufianmahin3@gmail.com>
2025-07-05 14:18:33 +0600

    added firebase accessToken authentication

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const port = process.env.PORT || 3000;
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const admin = require("firebase-admin");
 const stripe = require('stripe')(process.env.PAYMENT_GATEWAY_KEY);
+
 // middlewire
 app.use(cors());
 app.use(express.json());
 
+// Firebase Admin SDK
+const serviceAccount = require("./profast-firebase-adminsdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 
-// ==========================================================================================================================================
+// =====================================================================Mongo DB================================================================
 app.get('/', (req, res) => {
     res.send('Profast Delivery service is running')
 })
@@ -35,10 +42,61 @@ async function run() {
 
         const db = client.db("profast_delivery_db");
 
-        const parcelCollection = db.collection("parcelCollection");
+        const userCollection = db.collection("users");
+        const parcelCollection = db.collection("parcels");
+        const paymentCollection = db.collection("payments");
 
+        // custom middlewires
+        const verifyFBToken = async (req, res, next) => {
 
-        app.get('/parcels', async (req, res) => {
+            const authHeader = req.headers.Authorization;
+            if (!authHeader) {
+                return res.status(401).send({ message: "Unauthorized Access" })
+            }
+
+            const token = authHeader.split(" ")[1];
+            if (!token) {
+                return res.status(401).send({ message: "Unauthorized Access" })
+            }
+
+            try{
+                const decoded = await admin.auth().verifyIdToken();
+                req.decoded = decoded;
+                next();
+            }
+            catch{
+                return res.status(403).send({ message: "Forbidden Access" })
+            }
+
+            
+        }
+
+        app.post("/users", async (req, res) => {
+            const email = req.body.email;
+            const userExists = await userCollection.findOne({ email });
+
+            if (userExists) {
+                // update last logged in
+
+                const result = await userCollection.updateOne(
+                    { email },
+                    {
+                        $set: { last_log_in: new Date().toISOString() }
+                    }
+                )
+
+                return res.status(200).send({ message: "User Already exists", inserted: false })
+            }
+            else {
+                const userDetails = req.body;
+
+                const result = await userCollection.insertOne(userDetails);
+                res.send(result);
+            }
+        })
+
+        app.get('/parcels', verifyFBToken, async (req, res) => {
+
             try {
                 const userEmail = req.query.email;
                 const query = userEmail ? { 'senderDetails.email': userEmail } : {};
@@ -62,9 +120,49 @@ async function run() {
                 res.send(parcels);
 
             } catch (error) {
-                res.status(500).json({ message: 'Server error' });
+                res.status(500).send({ message: 'Server error' });
             }
         });
+
+
+
+        app.get('/payments', async (req, res) => {
+            const { email } = req.query;
+            try {
+                const query = email ? { 'payerEmail': email } : {};
+                const payments = await paymentCollection.find(query).sort({ paidAt: -1 }).toArray();
+
+                res.status(200).send(payments);
+            } catch (err) {
+                res.status(500).send({
+                    message: 'Failed to fetch payment history',
+                    error: err.message,
+                });
+            }
+        });
+
+        app.post('/payments', async (req, res) => {
+            const paymentData = req.body;
+
+            try {
+                // Update parcel payment_status to 'paid'
+                const updateQuery = { _id: new ObjectId(paymentData.parcelId) }
+                const updateResult = await parcelCollection.updateOne(updateQuery,
+                    {
+                        $set: { 'parcelDetails.payment_status': 'paid' }
+                    }
+                );
+
+                // add payment history
+                const paymentResult = await paymentCollection.insertOne(paymentData);
+
+                console.log(paymentResult);
+                res.send(paymentResult);
+
+            } catch (error) {
+                res.status(500).json({ message: 'Server error', error: error.message });
+            }
+        })
 
         app.post('/add-parcel', async (req, res) => {
             try {
@@ -72,11 +170,11 @@ async function run() {
                 const result = await parcelCollection.insertOne(parcelData);
                 res.status(200).send(result);
             } catch (error) {
-                res.status(500).json({ error: "Failed to add parcel" });
+                res.status(500).send({ error: "Failed to add parcel" });
             }
         })
 
-        // payment
+        // stripe payment intent 
         app.post('/create-payment-intent', async (req, res) => {
             const { amountInCents } = req.body;
 
